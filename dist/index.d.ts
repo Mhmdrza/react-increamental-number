913117024a55215a4ec8a47c0176cefd35f0f8ee
unknown <mr.ala72@gmail.com>
2020-09-01 22:13:57 +0430

    dist added to git ignore

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-interface IProps {
-    value: number;
-    totalSteps?: number;
-    className?: string;
-    outputFormatter?: (arg: number) => string | number;
-}
-declare const RafNumberCounter: ({ value, totalSteps, className, outputFormatter, }: IProps) => JSX.Element;
-export default RafNumberCounter;
